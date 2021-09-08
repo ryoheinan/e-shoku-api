8978132b132f4cddc72f76431d7e99bbe7e752a2
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2021-09-08 09:44:05 +0900

    Update models.py

--- app/models.py
+++ app/models.py
@@ -2,17 +2,22 @@ import uuid
 from django.db import models
 
 
-class User(models.Model):
+class CustomUser(models.Model):
     """ユーザーモデル"""
     class Meta:
-        db_table = 'user'
+        db_table = 'custom_user'
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(verbose_name='名前', max_length=20, unique=True)
-    address = models.CharField(
-        verbose_name='メールアドレス', max_length=30, unique=True)
+    username = models.CharField(
+        verbose_name='ユーザーネーム', max_length=20, unique=True)
+    display_name = models.CharField(verbose_name='名前', max_length=100)
+    password = models.CharField(
+        verbose_name='パスワード', max_length=128)
+    email = models.EmailField(
+        verbose_name='メールアドレス', max_length=256, unique=True)
     age = models.IntegerField(verbose_name='年齢', null=True, blank=True)
-    sex = models.CharField(verbose_name='性別', choices=('男性', '女性'))
+    gender = models.CharField(verbose_name='性別', choices=[
+        ('MALE', '男性'), ('FEMALE', '女性'), ('PNTS', '無回答'), ('OTHERS', 'その他')])
     created_at = models.DateTimeField(verbose_name='登録日時', auto_now_add=True)
 
 
@@ -22,11 +27,12 @@ class Room(models.Model):
         db_table = 'room'
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user = models.ManyToManyField(User, verbose_name='ユーザー')
-    role = models.CharField(verbose_name='役職', choices=('主催者', '参加者'))
+    user = models.ManyToManyField(CustomUser, verbose_name='ユーザー')
+    role = models.CharField(verbose_name='役職', choices=[
+                            ('HOST', '主催者'), ('PARTICIPANT', '参加者')])
     room_name = models.CharField(verbose_name='ルーム名', max_length=20)
-    members = models.IntegerField(verbose_name='人数', default=6, max_length=2)
+    members = models.IntegerField(verbose_name='人数', default=6)
     topic = models.TextField(verbose_name='トピック')
     invite_code = models.IntegerField(
-        verbose_name='招待コード', max_length=6, null=True, blank=True)
+        verbose_name='招待コード', null=True, blank=True)
     created_at = models.DateTimeField(verbose_name='登録日時', auto_now_add=True)
