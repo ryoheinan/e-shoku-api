1b32fcb467f281cdf08c109774aec15f66a3221e
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2021-09-08 10:25:15 +0900

    Create hashed store feature

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 import uuid
 from django.db import models
+from django.contrib.auth.hashers import make_password
 
 
 class CustomUser(models.Model):
@@ -16,9 +17,13 @@ class CustomUser(models.Model):
     email = models.EmailField(
         verbose_name='メールアドレス', max_length=256, unique=True)
     age = models.IntegerField(verbose_name='年齢', null=True, blank=True)
-    gender = models.CharField(verbose_name='性別', choices=[
+    gender = models.CharField(verbose_name='性別', max_length=6, choices=[
         ('MALE', '男性'), ('FEMALE', '女性'), ('PNTS', '無回答'), ('OTHERS', 'その他')])
     created_at = models.DateTimeField(verbose_name='登録日時', auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        self.password = make_password(self.password)
+        super(CustomUser, self).save(*args, **kwargs)
 
 
 class Room(models.Model):
@@ -28,8 +33,8 @@ class Room(models.Model):
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ManyToManyField(CustomUser, verbose_name='ユーザー')
-    role = models.CharField(verbose_name='役職', choices=[
-                            ('HOST', '主催者'), ('PARTICIPANT', '参加者')])
+    role = models.CharField(verbose_name='役職', max_length=5, choices=[
+                            ('HOST', '主催者'), ('GUEST', '参加者')])
     room_name = models.CharField(verbose_name='ルーム名', max_length=20)
     members = models.IntegerField(verbose_name='人数', default=6)
     topic = models.TextField(verbose_name='トピック')
