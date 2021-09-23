b78bf389c12db522d6a0830b42385436a4c6dc5c
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2021-09-23 16:20:04 +0900

    Create test_routing

--- api/urls.py
+++ api/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     # ユーザモデルの取得(詳細)・更新・一部更新・削除
     path('users/<pk>/', views.UserRetrieveUpdateDestroyAPIView.as_view()),
     # ルームモデルの取得(一覧)・登録
-    path('room/', views.RoomListCreateAPIView.as_view(), name='rooms'),
+    path('rooms/', views.RoomListCreateAPIView.as_view(), name='rooms'),
     # ルームモデルの取得(詳細)・更新・一部更新・削除
-    path('room/<pk>/', views.RoomRetrieveUpdateDestroyAPIView.as_view()),
+    path('rooms/<pk>/', views.RoomRetrieveUpdateDestroyAPIView.as_view()),
 ]
