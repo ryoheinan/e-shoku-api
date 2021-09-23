ca1c3b46346aca113ceae8c077c7fa0b1b36ae26
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2021-09-23 11:50:29 +0900

    Create test_users_api

---
+++ api/tests/test_users_api.py
@@ -0,0 +1,52 @@
+from rest_framework import status
+from rest_framework.test import APIClient, APITestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+
+
+USERS_URL = reverse('users')
+
+
+class PublicApiTests(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_auth_required(self):
+        """
+        認証必須になっているかどうかのテスト
+        """
+        data = {
+            'internalid': 'internalid',
+            'username': 'testuser',
+            'display_name': 'Test San',
+            'date_of_birth': '2020-01-01',
+            'gender': 'FEMALE',
+            'password': 'test_Secret!'
+        }
+        res = self.client.post(USERS_URL, data)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateApiTests(APITestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            'internalid', 'testuser', 'Test', '2020-01-01', 'FEMALE', 'test_Secret!')
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_users_post(self):
+        """
+        認証付きでユーザー作成可能かどうかのテスト
+        """
+        data = {
+            'internalid': 'internalid',
+            'username': 'testuser',
+            'display_name': 'Test San',
+            'date_of_birth': '2020-01-01',
+            'gender': 'FEMALE',
+            'password': 'Test_Secret!'
+        }
+        res = self.client.post(USERS_URL, data)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(get_user_model().objects.count(), 1)
+        self.assertEqual(get_user_model().objects.get().username, 'testuser')
--- api/urls.py
+++ api/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 urlpatterns = [
     # ユーザモデルの取得(一覧)・登録
-    path('users/', views.UserListCreateAPIView.as_view()),
+    path('users/', views.UserListCreateAPIView.as_view(), name='users'),
     # ユーザモデルの取得(詳細)・更新・一部更新・削除
     path('users/<pk>/', views.UserRetrieveUpdateDestroyAPIView.as_view()),
     # ルームモデルの取得(一覧)・登録
-    path('room/', views.RoomListCreateAPIView.as_view()),
+    path('room/', views.RoomListCreateAPIView.as_view(), name='rooms'),
     # ルームモデルの取得(詳細)・更新・一部更新・削除
     path('room/<pk>/', views.RoomRetrieveUpdateDestroyAPIView.as_view()),
 ]
