1f0e8c9aca06af256f2cce7cd3c30c14ed24614c
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2021-11-20 18:19:41 +0900

    Implement index func to urls.py

--- config/urls.py
+++ config/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Invalid URL")
+
 
 urlpatterns = [
-    # admin
+    path('', index),
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
